7861bfb9282d96fe97e75d593eda7620092fa3bf
ThursdayGamesDB <mickg77@hotmail.com>
2024-08-18 20:53:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
 import './App.css';
+import myVideo from './trees.mp4'
 
 function App() {
   return (
+
+    
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="video-container">
+    
+       <video controls loop autoPlay muted >
+        <source src={myVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="overlay-text">
+          <h3> “Gratitude is one of the sweet shortcuts to finding peace of mind and happiness inside.”</h3>
+          <h4> – Unknown</h4>
+        </div>
+        </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+import './App.css';
+import myVideo from './trees.mp4'
+
+function App() {
+  return (
+
+      <div>
+        
+        <h2>Home Page</h2>
+        <p>Welcome to the Home page.</p>
+      </div>
+  );
+}
+
+export default App;
